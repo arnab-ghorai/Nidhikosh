93f9b0c974789d7665de6607476e2fd6b00f4bd6
jilkumar-golakiya <jilkumargolakiya@gmail.com>
2024-08-15 14:04:18 +0530

    tailwind -- add

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="test">Nidhikosh</h1>
+      <h1 className="test-7xl text-center text-green-700">Nidhikosh</h1>
     </>
   );
 }
